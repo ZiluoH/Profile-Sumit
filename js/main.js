067afad21b535422ffab644340e78538122bfcf4
ZiluoH <ziluoh@gmail.com>
2017-05-16 21:20:48 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-var reg = /^[a-z0-9._%-]+@[a-z0-9.-]+\.[a-z]{2,5}$/
+var EmailInput = document.getElementById('Email')
+function EmailValid() {
+	var regEmail = /^[a-z0-9._%-]+@[a-z0-9.-]+\.[a-z]{2,5}$/
+	
+	var EmailError = document.createTextNode("wrong email")
+	if (!regEmail.test(EmailInput)) {
+       document.getElementById('ErrorEmail').appendChild(EmailError)
+	}
+    
+}
